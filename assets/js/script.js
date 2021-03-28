c504577eaadb9dbf75fd2b7c73a7eef794b65170
aaronh1986 <aaronharbourne@gmail.com>
2021-03-28 14:36:24 +0000

    Add text under venues image

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
 
 
 
-
+$(".concert-hall").click(function(){
+    //$(".concert-hall-below").addClass("height");
+    $(".concert-hall-below").addClass("height").html(`
+    <ul>
+    <li><h5 class="headers-below">Classical</h5></li>
+    <li><a href="https://www.nch.ie" target="_blank" class="list">The National Concert Hall (NCH)</a></li>
+    <li><h5 class="headers-below">Rock & Pop</h5></li>
+    <li><a href="https://www.vicarstreet.com" target="_blank" class="list">Vicar Street</a></li>
+    <li><a href="https://www.theacademydublin.com" target="_blank" class="list">The Academy</a></li>
+    <li><a href="https://www.3arena.ie" target="_blank" class="list">Three Arena</a></li>
+    <li><h5 class="headers-below">Jazz & Blues</h5></li>
+    <li><a href="https://www.arthurspub.ie" target="_blank" class="list">Arthurs, Blues & Jazz club</a></li>
+    </ul>
+    `);
+})
