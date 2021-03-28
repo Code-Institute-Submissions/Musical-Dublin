df44f9bcb881352274a9a6e8c6340748e7389fcb
aaronh1986 <aaronharbourne@gmail.com>
2021-03-28 16:52:44 +0000

    Add info under education image on index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function(){});
 
-$(".concert-hall").show(function(){
-    //$(".concert-hall-below").addClass("height");
-    $(".concert-hall-below").addClass("height").html(`
+$(".concert-hall").click(function(){
+    $(".images-below").html(`
     <ul>
     <li><h5 class="headers-below">Classical</h5></li>
     <li><a href="https://www.nch.ie" target="_blank" class="list">The National Concert Hall (NCH)</a></li>
@@ -13,9 +12,35 @@ $(".concert-hall").show(function(){
     <li><h5 class="headers-below">Jazz & Blues</h5></li>
     <li><a href="https://www.arthurspub.ie" target="_blank" class="list">Arthurs, Blues & Jazz club</a></li>
     </ul>
-    `);
+    `).scrollDown(10000);
 })
-});
+
+
+$(".education").click(function(){
+    $(".images-below").html(`
+    <ul>
+    <li><h5 class="headers-below">Jazz</h5></li>
+    <li><a href="https://www.newparkmusic.ie" target="_blank" class="list">Newpark Music Centre</a></li>
+    <li><h5 class="headers-below">Classical</h5></li>
+    <li><a href="https://www.ucd.ie/music/" target="_blank" class="list">UCD</a></li>
+    <li><h5 class="headers-below">Rock & Pop Performance</h5></li>
+    <li><a href="https://www.bimm.ie" target="_blank" class="list">BIMM</a></li>
+    <li><a href="https://www.rockjam.ie" target="_blank" class="list">Rock Jam</a></li>
+    <li><a href="https://www.rockschool.ie" target="_blank" class="list">Rock School</a></li>
+    </ul>`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
