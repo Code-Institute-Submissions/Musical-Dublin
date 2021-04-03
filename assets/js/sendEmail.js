15954bc9d1fa573f90d5f84d578ce32a73961677
aaronh1986 <aaronharbourne@gmail.com>
2021-04-03 11:06:23 +0000

    Form text-field validation added

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -11,5 +11,22 @@ function sendMail(contactForm) {
         console.log("EMAIL SEND FAIL", error);
     }
 )*/
-alert("We have received your listing. Thank you, we will reply shortly.");
+// alert("We have received your listing. Thank you, we will reply shortly.");
 }
+
+
+// Form validation code largely taken from Web Dev Simplified on YouTube, and modified for this project. Video credited in README file.
+const listing = document.getElementById('textarea');
+const form = document.getElementById('form');
+const errorDisplay = document.getElementById('error');
+
+form.addEventListener("submit", (e) => {
+    let errorMessage = [];  //This array will hold any error messages
+    if (listing.value === '' || listing.value == null) {
+        errorMessage.push('Please supply full details of your business including owners, address, website and/or social media links.')
+    }
+    if (errorMessage.length > 0){
+    e.preventDefault();
+    errorDisplay.innerText = errorMessage
+    }
+})
