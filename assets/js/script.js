535c1a1bf4a96dc24ab7a4bfdb68785b6e183b4c
aaronh1986 <aaronharbourne@gmail.com>
2021-04-13 22:34:22 +0000

    Add further specifications to api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,7 +147,7 @@ $(".shops").click(function(){
 
 
 function getInfo() {
-    fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
         .then(response => response.json()) //These are the promises to return response in JSON format
         .then((data) => sortInfo(data));
 }
@@ -163,7 +163,7 @@ function sortInfo(events) {
 
 
 
-
+/*https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK */
 
 
 
