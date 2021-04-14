f317c60c49e5484a164b70fbb86af467d5f44120
aaronh1986 <aaronharbourne@gmail.com>
2021-04-14 13:29:09 +0000

    Add updates to main.html

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,28 @@
 function getInfo() {
+    //GET request
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
+        .then(response => response.json()) //This promise returns response in JSON format
+        .then((data) => sortInfo(data));
+}
+
+
+document.getElementById('apibtn').addEventListener('click', getInfo);
+
+function sortInfo(events) {
+    console.log(events);
+    
+    }
+
+
+
+
+
+
+
+
+
+
+/*function getInfo() {
     fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
         .then(response => response.json()) //These are the promises to return response in JSON format
         .then((data) => sortInfo(data));
@@ -8,7 +32,7 @@ document.getElementById('apibtn').addEventListener('click', getInfo);
 
 function sortInfo(events) {
     console.log(events);
-    }
+    }*.
 
 
 
@@ -16,3 +40,5 @@ function sortInfo(events) {
 
 
 /*https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK */
+
+/*https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK */
