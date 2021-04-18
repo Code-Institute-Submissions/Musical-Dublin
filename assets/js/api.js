8fc1a273aa92d29a2283212dd299d942451b4777
aaronh1986 <aaronharbourne@gmail.com>
2021-04-18 19:18:08 +0000

    Add callback to api request

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,19 +1,24 @@
+function getData(cb){
 var xhr = new XMLHttpRequest();
-var data = 
+var data;
 
 xhr.open("GET", "https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
 xhr.send(); 
 
-function setData(jsonData){
-    data = jsonData;
-    console.log(data);
-}
-
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200 ) {
-        setData(JSON.parse(this.responseText));
+        cb(data = JSON.parse(this.responseText));
     }
 };
+}
+
+getData((data) => {
+        console.log(data);
+    });
+
+
+
+
 
 
 
