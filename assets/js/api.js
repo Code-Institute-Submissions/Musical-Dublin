a21a6b0fde18e1e57d0cd16d67d405186fd7e19e
aaronh1986 <aaronharbourne@gmail.com>
2021-04-18 16:45:10 +0000

    Get data from ticketmaster api using xhr

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,38 +1,59 @@
-function getInfo() {
+var xhr = new XMLHttpRequest();
+var data = 
+
+xhr.open("GET", "https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
+xhr.send(); 
+
+function setData(jsonData){
+    data = jsonData;
+    console.log(data);
+}
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200 ) {
+        setData(JSON.parse(this.responseText));
+    }
+};
+
+
+
+  
+
+
+
+/* document.getElementById('apiContainer').innerHTML =  
+console.log(JSON.parse(this.responseText));*/
+
+
+
+
+
+
+
+
+
+/* function getInfo() {
     //GET request
     fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
         .then(response => response.json()) //This promise returns response in JSON format
         .then((data) => sortInfo(data));
 }
 
-
-document.getElementById('apibtn').addEventListener('click', getInfo);
-
 function sortInfo(events) {
     console.log(events);
-    
-    }
-
-
-
-
-
-
-
-
-
-
-/*function getInfo() {
-    fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
-        .then(response => response.json()) //These are the promises to return response in JSON format
-        .then((data) => sortInfo(data));
 }
 
-document.getElementById('apibtn').addEventListener('click', getInfo);
+getInfo();   */
 
-function sortInfo(events) {
-    console.log(events);
-    }*.
+
+
+
+
+
+
+
+
+
 
 
 
