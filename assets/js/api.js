7a93a2226999a69755c278d75f1c07d378e10268
aaronh1986 <aaronharbourne@gmail.com>
2021-04-22 11:07:24 +0000

    Condense api code

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,27 +1,25 @@
 //GET request
 fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
 .then(response => response.json()) //This promise returns response in JSON format
-.then((data) => showEvents(data));    /* sortInfo(data) */
+.then((data) => displayEvents(data));    /* sortInfo(data) */
 
-function showEvents(json) {
-  var items = $('#events .list-group-item');
-  items.hide();
-  var events = json._embedded.events;
-  var item = items.first();
+function displayEvents(data) {
+  var items = $('.list-group-item');
+  var events = data._embedded.events;
+  var item = items.first(); 
  
-  for (var i=0;i<events.length;i++) {
+  for (var i = 0; i < events.length; i++) {
+    try {
     item.children('.list-group-item-heading').text(events[i].name);
     item.children('.list-group-item-text').text(events[i].dates.start.localDate);
     item.children('.status').text(events[i].dates.status.code);
     item.children('.promoter').text(events[i].promoters[1].name);
     item.children('.note').text(events[i].pleaseNote);
-
-  try {
-      item.children('.venue').text(events[i]._embedded.venues[0].name);
-    } catch (err) {
-      console.log(err);
+    item.children('.venue').text(events[i]._embedded.venues[0].name);
+    } catch (error) {
+      console.log(error);
     }
-    item.show();
+  item.show();
   console.log(events);
 } 
 }
