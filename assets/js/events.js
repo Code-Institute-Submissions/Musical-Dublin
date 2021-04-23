0e0c5aa6a3044d232d58a4c9cef7c02f665dff4f
aaronh1986 <aaronharbourne@gmail.com>
2021-04-23 13:46:20 +0000

    Show four events for api results

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,30 +1,32 @@
-//GET request
-/* fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
-.then(response => response.json()) //This promise returns response in JSON format
-.then((data) => displayEvents(data));   
+function makeApiCall(){
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK&city=['dublin']")
+.then(response => response.json())
+.then((data) => displayEvents(data))
+}
 
 function displayEvents(data) {
-  var item = $('.list-group-item');
+  var items = $('#events .list-group-item');
   var events = data._embedded.events;
- 
-for (var i = 0; i < events.length; i++) {
-    item.children('.list-group-item-heading').text(events[i].name);
-    item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+  var item = items.first();   
+  for (var i = 0; i < events.length; i++) {
+    item.children('.heading').text(events[i].name);
+    item.children('.date').text(events[i].dates.start.localDate);
+    try {
+    item.children('.venue').text(events[i]._embedded.venues[0].name);
     item.children('.status').text(events[i].dates.status.code);
     item.children('.promoter').text(events[i].promoters[1].name);
     item.children('.note').text(events[i].pleaseNote);
-    item.children('.venue').text(events[i]._embedded.venues[0].name);
-    } 
-  console.log(events);
-
+    item.children('.venue').text(events[i]._embedded.venues[0].name); 
+    } catch (err) {
+      console.log(err);
+    }
+   item=item.next();   
+  }
 }
 
- */
-
-
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK&city=['dublin']")
-.then(response => response.json())
-.then((data) => console.log(data))
+$(document).ready(function() {
+  makeApiCall();
+});
 
 
 
